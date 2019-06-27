ed3fa4518ad590b9626de1becacbe7aa09b08a1d
Cameron <cameronmeissner@gmail.com>
2019-06-27 10:40:06 -0700

    Created json dump script

---
+++ createData.py
@@ -0,0 +1,32 @@
+import requests as web
+import json
+
+endpoint = "http://127.0.0.1:5000"
+
+images = ["chocolate.jpg", "cookies.jpg", "idk.png", "salmon.jpg", "sauce.jpg", "sugar.jpg"]
+
+jsons = []
+
+for image in images:
+
+    image_path = 'images/{0}'.format(image)
+
+    files = {
+        'image': (image_path, open(image_path, 'rb')),
+    }
+
+    response = web.post(endpoint, files=files)
+    data = response.json()
+    jsons.append(data)
+
+with open('dump.json', 'w') as f:
+    json.dump(jsons, f, indent=4)
+
+print('done')
+
+
+
+
+
+
+
