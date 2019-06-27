279b11bd0132a82b9c7c1497fe363711cee0959a
pkelaita <pierce@kelaita.com>
2019-06-27 14:50:10 -0700

    yee

--- ItemHandler.py
+++ ItemHandler.py
@@ -1,11 +1,47 @@
-import db
+from bscanner import bscan_fs
+import json
 
 JSON = 0
 PRICE = 1
 
 
-def get_item_information():
-    pass
+def get_item_information(img_fs, price):
+    item = bscan_fs(img_fs)
+
+    # TODO configure SQL DB
+    with open('data/stores.json') as f:
+        stores = json.load(f)
+    with open('data/items.json') as f:
+        items = json.load(f)
+
+    # Find the matching item
+    match = None
+    for it in items:
+        if it['product_name'] == item['product_name'] and it['brands'] == item['brands']:
+            match = it
+    if not match:
+        return {'error': 'product not found in any stores'}
+
+    # Map name to stores
+    store_map = {}
+    for it in stores:
+        store_map[it['store_name']] = it
+
+    # Filter stores
+    tuple_list = []
+    for store_name in match['stores']:
+        tup = (
+            store_map[store_name],
+            match['wsp'] * store_map[store_name]['markup']
+        )
+        tuple_list.append(tup)
+
+    ranked = rank(tuple_list)
+    result = {}
+    for key in ranked:
+        if float(ranked[key][PRICE]) < float(price):
+            result[key] = ranked[key]
+    return result
 
 
 def rank(store_data_list):
@@ -30,10 +66,10 @@ def rank(store_data_list):
     assert len(norm_distances) == len(distances)
 
     for i in range(len(store_data_list)):
-        store_name = store_data_list[i][JSON]['name']
+        store_name = store_data_list[i][JSON]['store_name']
         distance = norm_distances[i]
         price = norm_prices[i]
-        ranks[store_name] = calculate_rank(distance, price)
+        ranks[store_name] = (calculate_rank(distance, price), store_data_list[i][PRICE])
 
     return ranks
 
@@ -47,15 +83,10 @@ def calculate_rank(distance, price):
     :param distance: distance to specific store
     :return: ranking based on distance and store
     """
-    return 0.7*price + 0.3*distance
+    return 0.7 * price + 0.3 * distance
 
 
 def normalize(data):
     max_ = max(data)
     normalized = [data[i] / max_ for i in range(len(data))]
     return normalized
-
-
-
-
-
